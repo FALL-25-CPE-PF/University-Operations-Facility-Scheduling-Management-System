26e1f12a7005d555ee1e29a83e4fe8d12a3315e0
unknown <sairahshabbir037@gmail.com>
2026-01-02 09:53:40 -0800

    Created class facility structure

---
+++ University_Facility_Management_System.py
@@ -0,0 +1,8 @@
+class Facility:
+    def _init_(self):
+        self.fid = None
+        self.name = None
+        self.capacity = None
+        self.days = []
+        self.slots = []
+        self.status = "Active"
