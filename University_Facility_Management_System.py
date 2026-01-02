a371cf2235da4b412ca9cbefe9e51e7f06389553
Ayeshashabir778 <ayesha486ooo@gmail.com>
2026-01-02 09:30:58 -0800

    Implemented teacher menu

--- University_Facility_Management_System.py
+++ University_Facility_Management_System.py
@@ -2,3 +2,28 @@ class Teacher(User):
 
     def _init_(self):
         super()._init_()
+    def teacher_menu(self):
+        while True:
+            print("\n====== TEACHER MODE ======")
+            print("1. Request Lecture / Lab")
+            print("2. View My Requests")
+            print("3. View My Schedule")
+            print("4. Logout")
+
+            choice = input("Select option: ")
+
+            if choice == "1":
+                self.request_facility()
+
+            elif choice == "2":
+                self.view_my_requests()
+
+            elif choice == "3":
+                self.view_my_schedule()
+
+            elif choice == "4":
+                print("Teacher logged out")
+                break
+
+            else:
+                print("Invalid option")
