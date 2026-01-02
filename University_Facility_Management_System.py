6e8cd309a17b89774592efe34960190b43b0cf65
Sairahshabir <sairahshabir037@gmail.com>
2026-01-02 10:56:56 -0800

    created class facility structure

--- University_Facility_Management_System.py
+++
@@ -1,8 +0,0 @@
-class Facility:
-    def _init_(self):
-        self.fid = None
-        self.name = None
-        self.capacity = None
-        self.days = []
-        self.slots = []
-        self.status = "Active"
