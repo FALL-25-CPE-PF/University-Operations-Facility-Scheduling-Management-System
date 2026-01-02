bff026a3a5ca4fe337ff21148be21c85c034de87
chjunaidsalem <chjunaidsaleem7@gmail.com>
2026-01-02 13:43:54 -0800

    Add user registration feature

--- University_Facility_Management_System.py
+++ University_Facility_Management_System.py
@@ -8,4 +8,32 @@ class User:
         self.name = name
         self.id = id
         self.role = role
+    
+    def register_user(self):
+        field = ["username","password","role"]
+        username = input("Enter Username: ")
+        file_exist = os.path.exists("user_login.csv")
         
+        if file_exist:
+            with open("user_login.csv", "r") as file:
+                reader = csv.DictReader(file)
+                for user in reader:
+                    if username == user["username"]:
+                        print("Username already exist!")
+                        return
+                    
+        password = input("Enter Password: ")
+        role = input("Enter your Role: ").lower()
+        user_found = False
+
+        if not user_found:
+            with open("user_login.csv","a",newline="") as file:
+                writer = csv.DictWriter(file,fieldnames=field)
+                if not file_exist:
+                    writer.writeheader()
+                writer.writerow({
+                    "username": username,
+                    "password": password,
+                    "role": role
+                    })
+                print("Registered Successfully!")
