e31c179ad2e5b21ceda295b88e39b8d5842700b1
Ayeshashabir778 <ayesha486ooo@gmail.com>
2026-01-04 09:39:04 -0800

    Implemented student menu

--- University_Facility_Management_System.py
+++ University_Facility_Management_System.py
@@ -102,3 +102,28 @@ class Student(User):
 
     def _init_(self):
         super()._init_()   # username, password, role
+    def student_menu(self):
+        while True:
+            print("\n====== STUDENT MODE ======")
+            print("1. Request Facility")
+            print("2. View My Requests")
+            print("3. Cancel Pending Request")
+            print("4. Logout")
+
+            choice = input("Select option: ")
+
+            if choice == "1":
+                self.request_facility()
+
+            elif choice == "2":
+                self.view_my_requests()
+
+            elif choice == "3":
+                self.cancel_pending_request()
+
+            elif choice == "4":
+                print("Student logged out")
+                break
+
+            else:
+                print("Invalid option")
